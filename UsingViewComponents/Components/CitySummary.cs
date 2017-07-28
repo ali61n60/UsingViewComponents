7109914b83d54632707c92dd455d7007222f7f2a
Ali <ali62n62@yahoo.com>
2017-07-28 15:44:45 +0430

    return HtmlString

--- UsingViewComponents/Components/CitySummary.cs
+++ UsingViewComponents/Components/CitySummary.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using UsingViewComponents.Models;
 
 namespace UsingViewComponents.Components
@@ -18,7 +20,8 @@ namespace UsingViewComponents.Components
 
         public IViewComponentResult Invoke()
         {
-            return Content("This is a <h3><i>string</i></h3>");
+            return new HtmlContentViewComponentResult(
+                new HtmlString("This is a <h3><i>string</i></h3>"));
         }
     }
 }
