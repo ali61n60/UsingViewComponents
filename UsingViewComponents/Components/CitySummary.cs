8007d70ff385937d8156de1fffcc7780e4704b04
Ali <ali62n62@yahoo.com>
2017-07-28 15:03:05 +0430

    deriving from ViewComponent class 1st example

---
+++ UsingViewComponents/Components/CitySummary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Components
+{
+    public class CitySummary : ViewComponent
+    {
+        private ICityRepository repository;
+        public CitySummary(ICityRepository repo)
+        {
+            repository = repo;
+        }
+        public string Invoke()
+        {
+            return $"{repository.Cities.Count()} cities, "
+                   + $"{repository.Cities.Sum(c => c.Population)} people";
+        }
+    }
+}
